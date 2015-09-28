1350bfced251a52d10f603984e0cd282f112dc6c
Livio Ribeiro <livioribeiro@outlook.com>
2015-09-28 09:09:42 -0300

    added string_len examples

--- luajit-to-rust/src/main.lua
+++ luajit-to-rust/src/main.lua
@@ -10,12 +10,20 @@ end
 
 ffi.cdef[[
 int32_t double_input(int32_t input);
+int32_t string_len(char* input);
 ]]
 
-local lib = ffi.load('target/debug/libdouble_input.' .. ext)
+local lib = ffi.load('target/debug/libffi_example.' .. ext)
 local double_input = lib.double_input
+local string_len = lib.string_len
 
 local input = 4
 local output = double_input(input)
+print('[double_input(int) -> int]')
+print(input .. ' * 2 = ' .. output)
+print()
 
-print(input .. " * 2 = " .. output)
+input = 'Hello, FFI!'
+output = string_len(ffi.new('char[?]', string.len(input), input))
+print('[string_len(string) -> int]')
+print(string.format('Input: %s\nLua len: %d\nRust len: %d', input, string.len(input), output))
